520783dfdbd727bf0a02c151cead45e2563e3991
Alex Kim <askim1019@gmail.com>
2018-06-28 00:47:58 -0500

    Fix connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 var mysql = require("mysql");
 var keys = require("../keys");
-var connection
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connect = mysql.createConnection(process.env.JAWSDB_URL);
