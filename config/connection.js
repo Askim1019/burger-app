eaabd2aaa71823872ac31723ce05604acb0b8cc5
Alex Kim <askim1019@gmail.com>
2018-06-28 17:46:38 -0500

    Fix connection variable for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var keys = require("../keys");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connect = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
